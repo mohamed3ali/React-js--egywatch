251c91248506903902cc56c6308b30b35772e076
MOHAMED011111 <moedali094@gmail.com>
2022-07-26 23:49:43 +0200

    Updates

---
+++ precache-manifest.d8c37a46b1a41ba6107bd898f49d6f2c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ec3fabf0eebb058d3e4",
+    "url": "/egywatch/static/css/main.16e552fd.chunk.css"
+  },
+  {
+    "revision": "4ec3fabf0eebb058d3e4",
+    "url": "/egywatch/static/js/main.4ec3fabf.chunk.js"
+  },
+  {
+    "revision": "6645e0859c07f3ceefad",
+    "url": "/egywatch/static/js/1.6645e085.chunk.js"
+  },
+  {
+    "revision": "30cdfb387a51517da27d",
+    "url": "/egywatch/static/js/runtime~main.30cdfb38.js"
+  },
+  {
+    "revision": "1da46f78c37d55f9580732539527f285",
+    "url": "/egywatch/static/media/3.1da46f78.jpg"
+  },
+  {
+    "revision": "7e6979b74592edf15fcc43a41d0d33b0",
+    "url": "/egywatch/static/media/ll.7e6979b7.jpg"
+  },
+  {
+    "revision": "607caefe4be6be78cfb5cd8db254a395",
+    "url": "/egywatch/static/media/yy.607caefe.jpg"
+  },
+  {
+    "revision": "40e30cf08bf88f42bec40be5e35e33f0",
+    "url": "/egywatch/static/media/o.40e30cf0.jpg"
+  },
+  {
+    "revision": "9002737b693f990cf10580c08aa9b2bc",
+    "url": "/egywatch/static/media/1.9002737b.jpg"
+  },
+  {
+    "revision": "5bbeaa16263e39635c7a1e2af71e49b0",
+    "url": "/egywatch/static/media/4.5bbeaa16.jpg"
+  },
+  {
+    "revision": "367c391ba7ba2aeba5caa18dfbfff324",
+    "url": "/egywatch/static/media/ss.367c391b.svg"
+  },
+  {
+    "revision": "765c42baeb008f0c7f52f350ae8e632a",
+    "url": "/egywatch/static/media/cc.765c42ba.svg"
+  },
+  {
+    "revision": "5e548b2d44dd6b4a6950f2053d5faa44",
+    "url": "/egywatch/static/media/cart.5e548b2d.svg"
+  },
+  {
+    "revision": "dbcc9acec1305377f605be384786f861",
+    "url": "/egywatch/index.html"
+  }
+];
